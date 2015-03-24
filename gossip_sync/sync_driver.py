e1d616e53b47a81263a3f54ebb5d179bf4945e35
Clark GREDONA <cgredona@gmail.com>
2015-03-24 16:59:12 -0400

    Refactor and clean up sync_driver.py.  Move matrix util to separate module

--- gossip_sync/sync_driver.py
+++ gossip_sync/sync_driver.py
@@ -10,48 +10,23 @@ import numpy as np
 import sys
 import time
 
+import matrix_util
+
 # Constants
 NUM_LINES_IN_FILE = 100
 EPSILON = 1E-7
 LEARNING_RATE = 1E-4
+MAX_ITERATIONS = 1E6
+
+DATA_LOCATION = "../line_data2.txt"
+
 P = np.array([[(1.0/3.0), (1.0/3.0), 0.0, (1.0/3.0)], [(1.0/3.0), (1.0/3.0),
 (1.0/3.0), 0.0], [0.0, (1.0/3.0), (1.0/3.0), (1.0/3.0)], [(1.0/3.0), 0.0, (1.0/3.0), (1.0/3.0)]]) 
-MAX_ITERATIONS = 1E6
 
 # 1/3 1/3 0 1/3
 # 1/3 1/3 1/3 0
 # 0 1/3 1/3 1/3
 # 1/3 0 1/3 1/3
-
-def isStochastic(vector):
-    """Returns true if numpy vector is stochastic"""
-	
-    # if not equal to 1 within machine precision
-    if abs(np.sum(vector) - 1) < sys.float_info.epsilon:
-        return True
-
-def isColumnStochastic(P):
-    """Returns true if 2D array P is column stochastic"""
-    assert P.ndim >= 2
-    # iterate over columns by iterating over rows of the transpose
-    for column in P.T:
-        if not isStochastic(column):
-            return False
-    return True	
-
-def isRowStochastic(P):
-    """Returns true if 2D array P is column stochastic"""
-    assert P.ndim >= 2
-	
-    for row in P:
-        if not isStochastic(row):
-            return False
-    return True	
-
-def isDoublyStochastic(P):	
-    """Returns true if 2D array P is doubly stochastic"""
-    assert P.ndim >= 2
-    return isColumnStochastic(P) and isRowStochastic(P)
 
 def gradient_descent_runner(training_data, w, LEARNING_RATE, num_iterations):
     for i in range(num_iterations):
@@ -83,109 +58,83 @@ def gradient(w, training_data):
 def hasConverged(old_w, w, EPSILON, num_iterations, max_iterations):
     return (abs(w[0] - old_w[0]) < EPSILON and abs(w[1] - old_w[1])) or (num_iterations > max_iterations)
 
-# Initialize MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+def run():
+    # Initialize MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-# Partition data
-linesPerTask = NUM_LINES_IN_FILE / size
-startLine = rank * linesPerTask
-endLine = (rank + 1) * linesPerTask - 1
+    # Partition data
+    lines_per_task = NUM_LINES_IN_FILE / size
+    start_line = rank * lines_per_task
+    end_line = (rank + 1) * lines_per_task - 1
 
-currLine = 0
-dataFile = open("../line_data2.txt", "r")
-training_data = []
-for line in dataFile:
-	if currLine >= startLine and currLine <= endLine:
-		x, y = [float(f) for f in line.split(" ")]
-		training_data.append(np.array([x, y]))	
-	currLine += 1
+    curr_line = 0
+    data_file = open(DATA_LOCATION, "r")
+    training_data = []
+    for line in data_file:
+        if curr_line >= start_line and curr_line <= end_line:
+            x, y = [float(f) for f in line.split(" ")]
+            training_data.append(np.array([x, y]))	
+        curr_line += 1
 
-# Init
-w = np.array([0.0, 0.0])
-q = np.array([0.0, 0.0])
+    # Init
+    w = np.array([0.0, 0.0])
+    q = np.array([0.0, 0.0])
 
-# print("rank {0} beginning\n".format(rank))
+    num_iterations = 0
 
-# starting_b = 0
-# starting_m = 0
-# [m,b] = gradient_descent_runner(training_data, w, LEARNING_RATE, 100000)
-# print("After 100000 iterations, b = {0}, m = {1}".format(b,m))
+    converged = False
+    while not converged:
 
-num_iterations = 0
+        old_w = w
+        if rank == 0:
+            print("\niteration {0}".format(num_iterations))
+            print("current model: m: {1}, b: {2}".format(rank,w[0],w[1]))
 
+        q = w - LEARNING_RATE * gradient(w, training_data)
 
-converged = False
-while not converged:
-
-    old_w = w
-    if rank == 0:
-        print("\niteration {0}".format(num_iterations))
-        print("current model: m: {1}, b: {2}".format(rank,w[0],w[1]))
-
-    # if rank == 0:
-    #     grad_w = gradient(w, training_data)
-    #     print("node {0} computing grad w...".format(rank))
-    #     print("grad m at node {0} = {1}".format(rank,grad_w[0]))
-    #     print("grad b at node {0} = {1}".format(rank,grad_w[1]))
-    q = w - LEARNING_RATE * gradient(w, training_data)
-
-    # send q to other nodes
-    for u in range(0, size):
-        if rank != u:
-            comm.Isend([q, MPI.FLOAT], u, tag = num_iterations)
-
-    # collect q from other nodes
-    q_u = [np.empty(q.size, dtype=np.float64) for i in range(0, size)]
-    q_u[rank] = q
-
-    status = MPI.Status()
-    number_of_messages_received = 0
-
-    # while number_of_messages_received != (size - 1):
-    #     while not comm.Iprobe(source=MPI.ANY_SOURCE, tag = num_iterations, status = status):
-    #         # create empty buffer and determine which node the message came from
-    #         # (so we know which Puv to apply to it)
-    #         pass
-
-    #     new_data = np.empty(w.size, dtype=np.float64)
-    #     u = status.source
-            
-    #     # pop from the message queue
-    #     comm.Irecv([new_data, MPI.FLOAT], source = MPI.ANY_SOURCE, tag = num_iterations) 
-    #     q_u[u] = new_data
-    #     if rank == 0:
-    #         print("Received {0} from node {1} during node {1}'s iteration {2}".format(new_data,u,status.tag))
-
-    #     number_of_messages_received += 1
-
-    # without this barrier, only 0 gets sent...
-    comm.Barrier()
-
-    while number_of_messages_received != (size - 1):
-        #pop from the message queue
+        # send q to other nodes
         for u in range(0, size):
-            new_data = np.empty(q.size, dtype=np.float64)
             if rank != u:
-                rcvd_from_u = comm.Irecv([new_data, MPI.FLOAT], source = u, tag = num_iterations)
+                comm.Isend([q, MPI.FLOAT], u, tag = num_iterations)
 
-                if rcvd_from_u:
-                    q_u[u] = new_data
-                    number_of_messages_received += 1
-                    
+        # collect q from other nodes
+        q_u = [np.empty(q.size, dtype=np.float64) for i in range(0, size)]
+        q_u[rank] = q
 
-    
-    w = np.array([0., 0.])
-    for u in range(0, size):
-        Puv = P[rank, u]
-        w += q_u[u] * Puv
+        status = MPI.Status()
+        number_of_messages_received = 0
 
-    if rank == 0:
-        print("new model: {0}\n".format(w))
+        # without this barrier, only 0 gets sent...
+        comm.Barrier()
 
-    # wait for all nodes to finish this iteration
-    comm.Barrier()
-    num_iterations += 1
+        while number_of_messages_received != (size - 1):
+            #pop from the message queue
+            for u in range(0, size):
+                new_data = np.empty(q.size, dtype=np.float64)
+                if rank != u:
+                    rcvd_from_u = comm.Irecv([new_data, MPI.FLOAT], source = u, tag = num_iterations)
 
-    converged = hasConverged(old_w, w, EPSILON, num_iterations, MAX_ITERATIONS)
+                    if rcvd_from_u:
+                        q_u[u] = new_data
+                        number_of_messages_received += 1
+                        
+
+        
+        w = np.array([0., 0.]) # zero out w
+        for u in range(0, size):
+            Puv = P[rank, u]
+            w += q_u[u] * Puv
+
+        if rank == 0:
+            print("new model: {0}\n".format(w))
+
+        # wait for all nodes to finish this iteration
+        comm.Barrier()
+        num_iterations += 1
+
+        converged = hasConverged(old_w, w, EPSILON, num_iterations, MAX_ITERATIONS)
+
+if __name__ == '__main__':
+    run()
