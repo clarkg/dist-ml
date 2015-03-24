ed5914d9c6fa3adf2a02adb09ccd122081f07746
Loren Lugosch <loren.lugosch@gmail.com>
2015-03-24 14:30:05 -0400

    Communication fixed

--- gossip_sync/sync_driver.py
+++ gossip_sync/sync_driver.py
@@ -11,9 +11,9 @@ import sys
 import time
 
 # Constants
-NUM_LINES_IN_FILE = 100000
+NUM_LINES_IN_FILE = 100
 EPSILON = 1E-6
-LEARNING_RATE = 1E-9
+LEARNING_RATE = 1E-3
 P = np.array([[(1.0/3.0), (1.0/3.0), 0.0, (1.0/3.0)], [(1.0/3.0), (1.0/3.0),
 (1.0/3.0), 0.0], [0.0, (1.0/3.0), (1.0/3.0), (1.0/3.0)], [(1.0/3.0), 0.0, (1.0/3.0), (1.0/3.0)]]) 
 
@@ -56,13 +56,18 @@ def isDoublyStochastic(P):
 def gradient(w, training_data):
     m_gradient = 0.
     b_gradient = 0.
-    for x, y in training_data:
-        # w[0] is m and w[1] is b
-        m_gradient += -2 * (y - (w[0] * x) - w[1]) * x
-        b_gradient += -2 * (y - (w[0] * x) - w[1]) 
+    N = len(training_data)
+    m = w[0]
+    b = w[1]
 
-    m_gradient /= len(training_data)
-    b_gradient /= len(training_data)
+    for x, y in training_data:
+        # if rank == 0: print("x: {0}, y: {1}".format(x,y))
+        # w[0] is m and w[1] is b
+        m_gradient += (-2/N) * x * (y - (m * x) - b)
+        b_gradient += (-2/N) * (y - (m * x) - b) 
+
+    # m_gradient /= len(training_data)
+    # b_gradient /= len(training_data)
 
     return np.array([m_gradient, b_gradient])
 
@@ -77,7 +82,7 @@ startLine = rank * linesPerTask
 endLine = (rank + 1) * linesPerTask - 1
 
 currLine = 0
-dataFile = open("../line_data.txt", "r")
+dataFile = open("../line_data2.txt", "r")
 training_data = []
 for line in dataFile:
 	if currLine >= startLine and currLine <= endLine:
@@ -87,13 +92,22 @@ for line in dataFile:
 
 # Init
 w = np.array([0.0, 0.0])
-q = 0.0
+q = np.array([0.0, 0.0])
 
-print("rank {0} beginning\n".format(rank))
+# print("rank {0} beginning\n".format(rank))
 
 num_iterations = 0
 
 while num_iterations < 10:
+    if rank == 0:
+        print("\niteration {0}".format(num_iterations))
+        print("current model: m: {1}, b: {2}".format(rank,w[0],w[1]))
+
+    # if rank == 0:
+    #     grad_w = gradient(w, training_data)
+    #     print("node {0} computing grad w...".format(rank))
+    #     print("grad m at node {0} = {1}".format(rank,grad_w[0]))
+    #     print("grad b at node {0} = {1}".format(rank,grad_w[1]))
     q = w - (LEARNING_RATE * gradient(w, training_data))
 
     # send q to other nodes
@@ -108,29 +122,46 @@ while num_iterations < 10:
     status = MPI.Status()
     number_of_messages_received = 0
 
-    while number_of_messages_received != (size - 1):
-        while not comm.Iprobe(source=MPI.ANY_SOURCE, tag = num_iterations, status = status):
-            # create empty buffer and determine which node the message came from
-            # (so we know which Puv to apply to it)
-            pass
+    # while number_of_messages_received != (size - 1):
+    #     while not comm.Iprobe(source=MPI.ANY_SOURCE, tag = num_iterations, status = status):
+    #         # create empty buffer and determine which node the message came from
+    #         # (so we know which Puv to apply to it)
+    #         pass
 
-        new_data = np.empty(w.size, dtype=np.float64)
-        u = status.source
+    #     new_data = np.empty(w.size, dtype=np.float64)
+    #     u = status.source
             
-        # pop from the message queue
-        comm.Irecv([new_data, MPI.FLOAT], source = MPI.ANY_SOURCE, tag = num_iterations) 
-        q_u[u] = new_data
-        if rank == 0:
-            print("Received {0} from node {1} during node {1}'s iteration {2}".format(new_data,u,status.tag))
+    #     # pop from the message queue
+    #     comm.Irecv([new_data, MPI.FLOAT], source = MPI.ANY_SOURCE, tag = num_iterations) 
+    #     q_u[u] = new_data
+    #     if rank == 0:
+    #         print("Received {0} from node {1} during node {1}'s iteration {2}".format(new_data,u,status.tag))
 
-        number_of_messages_received += 1
+    #     number_of_messages_received += 1
 
+    # without this barrier, only 0 gets sent...
+    comm.Barrier()
+
+    while number_of_messages_received != (size - 1):
+        #pop from the message queue
+        for u in range(0, size):
+            new_data = np.empty(q.size, dtype=np.float64)
+            if rank != u:
+                rcvd_from_u = comm.Irecv([new_data, MPI.FLOAT], source = u, tag = num_iterations)
+
+                if rcvd_from_u:
+                    q_u[u] = new_data
+                    number_of_messages_received += 1
+                    if rank == 0:
+                        print("Node {1}'s q: {0}".format(new_data,u))
+
+    print("Node 0's q: {}".format(q))
     for u in range(0, size):
         Puv = P[rank, u]
         w += q_u[u] * Puv
 
     if rank == 0:
-        print("{0}\n".format(w))
+        print("new model: {0}\n".format(w))
 
     # wait for all nodes to finish this iteration
     comm.barrier()
