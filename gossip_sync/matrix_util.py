e80177c07e21f42438fc73fd7fba21b11d4b2851
Clark GREDONA <cgredona@gmail.com>
2015-04-03 00:16:32 -0400

    Beautify matrix util

--- gossip_sync/matrix_util.py
+++ gossip_sync/matrix_util.py
@@ -1,12 +1,14 @@
 import numpy as np
 import sys
 
+
 def isStochastic(vector):
     """Returns true if numpy vector is stochastic"""
-	
+
     # if not equal to 1 within machine precision
     if abs(np.sum(vector) - 1) < sys.float_info.epsilon:
         return True
+
 
 def isColumnStochastic(P):
     """Returns true if 2D array P is column stochastic"""
@@ -15,21 +17,24 @@ def isColumnStochastic(P):
     for column in P.T:
         if not isStochastic(column):
             return False
-    return True	
+    return True
+
 
 def isRowStochastic(P):
     """Returns true if 2D array P is column stochastic"""
     assert P.ndim >= 2
-	
+
     for row in P:
         if not isStochastic(row):
             return False
-    return True	
+    return True
 
-def isDoublyStochastic(P):	
+
+def isDoublyStochastic(P):
     """Returns true if 2D array P is doubly stochastic"""
     assert P.ndim >= 2
     return isColumnStochastic(P) and isRowStochastic(P)
+
 
 def isSquare(P):
     """Returns true if array P is a square array"""
