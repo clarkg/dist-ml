cdaedd28105828d1581a6b0ae4b14825c7ff45e6
Loren Lugosch <loren.lugosch@gmail.com>
2015-04-04 15:44:49 -0400

    Less memory-hungry version of training data generator

--- create_multivariate_training_data.m
+++ create_multivariate_training_data.m
@@ -1,20 +1,25 @@
 % create random training set for 
 % multivariate linear regression test
 
-% d-dimensional m vector
-d = 20;
+function [m,b] = create_multivariate_training_data(d,N)
+
+% d-dimensional m vector, b
+% write both to disk
 m = 10 * randn([1 d]);
 b = randn([1 1]);
+params = horzcat(m,b);
+params_filename = sprintf('multivariate_line_params_d%d_n%d.txt',d,N);
+dlmwrite(params_filename,params,' ');
+
+% prepare data file for writing
+data_filename = sprintf('multivariate_line_data_d%d_n%d.txt',d,N);
 
 % create N lines of training data
-N = 1000000;
-X = 10*randn([N d]);
-Y = zeros([N 1]);
-
+% (forget each line after writing to disk)
 for i = 1:N
-    Y(i,:) = m*X(i,:)' + b;
+    X = 10*randn([1 d]);
+    Y = zeros([1 1]);
+    Y = m*X' + b;
+    Z = horzcat(X,Y);
+    dlmwrite(data_filename,Z,'-append','delimiter',' ');
 end
-
-% horizontally concatenate X and Y
-Z = horzcat(X,Y);
-dlmwrite('multivariate_line_data.txt',Z,' ');
